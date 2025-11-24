02d9ba2be6d33a4a37c7140cafe7c18dbd9d3530
Shinnosuke Iwaide <shinnosuke_iwaide@linc-well.com>
2025-11-24 15:01:25 +0900

    切り出した

--- app/models/concerns/scope_linker.rb
+++ app/models/concerns/scope_linker.rb
@@ -3,55 +3,52 @@ module ScopeLinker
 
   class_methods do
     def link_scope_predicate(scope_name, predicate: "#{scope_name}?")
-      raise ArgumentError, "Scope #{scope_name} is not defined" unless respond_to?(scope_name)
-      raise ArgumentError, "Predicate method #{predicate} is not defined" unless instance_methods.include?(predicate.to_sym)
-      singleton_class.define_method("linked_#{scope_name}") do |*args, &blk|
-        scoped_records = self.public_send(scope_name, *args, &blk)
-      end
-      # Relation 用のモジュールを作成。Model.all で返る relation をこのモジュールで拡張する。
-      relation_module = Module.new do
-        define_method("linked_#{scope_name}") do |*args, &blk|
-          # self は ActiveRecord::Relation のインスタンス、klass はモデルクラス
-          model_scope = klass.public_send(scope_name, *args, &blk)
-
-          # relation がロード済みなら Ruby 側で predicate を実行して配列を返す（既存コメントの方針）
-          if loaded?
-            unless klass.instance_methods.include?(predicate.to_sym)
-              raise ArgumentError, "Predicate method #{predicate} is not defined on #{klass}"
-            end
-            to_a.select { |rec| rec.public_send(predicate, *args) }
-          else
-            public_send(scope_name, *args, &blk)
-          end
+      ensure_scope_linkable!(scope_name) do |klass|
+        unless klass.instance_methods.include?(predicate.to_sym)
+          raise ArgumentError, "Predicate method #{predicate} is not defined"
         end
       end
 
-      # Model.all が返す relation を常に relation_module で拡張するようにする
-      singleton_class.class_eval do
-        define_method(:all) do
-          super().extending(relation_module)
-        end
-      end
+      define_linked_scope(
+        scope_name,
+        apply_loaded: ->(relation, args, _blk) {
+          relation.select { |rec| rec.public_send(predicate, *args) }
+        }
+      )
     end
 
     def link_scope_filter(scope_name, filter: "#{scope_name}_filter")
-      raise ArgumentError, "Scope #{scope_name} is not defined" unless respond_to?(scope_name)
-      raise ArgumentError, "Filter method #{filter} is not defined" unless respond_to?(filter)
-      singleton_class.define_method("linked_#{scope_name}") do |*args, &blk|
-        scoped_records = self.public_send(scope_name, *args, &blk)
+      ensure_scope_linkable!(scope_name) do |klass|
+        unless klass.respond_to?(filter)
+          raise ArgumentError, "Filter method #{filter} is not defined"
+        end
       end
-      # Relation 用のモジュールを作成。Model.all で返る relation をこのモジュールで拡張する。
+
+      define_linked_scope(
+        scope_name,
+        apply_loaded: ->(relation, _args, _blk) {
+          relation.klass.public_send(filter, relation.to_a)
+        }
+      )
+    end
+
+    private
+
+    def ensure_scope_linkable!(scope_name)
+      raise ArgumentError, "Scope #{scope_name} is not defined" unless respond_to?(scope_name)
+      yield(self) if block_given?
+    end
+
+    def define_linked_scope(scope_name, apply_loaded:)
+
+      singleton_class.define_method("linked_#{scope_name}") do |*args, &blk|
+        public_send(scope_name, *args, &blk)
+      end
+
       relation_module = Module.new do
         define_method("linked_#{scope_name}") do |*args, &blk|
-          # self は ActiveRecord::Relation のインスタンス、klass はモデルクラス
-          model_scope = klass.public_send(scope_name, *args, &blk)
-
-          # relation がロード済みなら Ruby 側で filter を実行して配列を返す（既存コメントの方針）
           if loaded?
-            unless klass.respond_to?(filter)
-              raise ArgumentError, "Filter method #{filter} is not defined on #{klass}"
-            end
-            klass.public_send(filter, to_a)
+            apply_loaded.call(self, args, blk)
           else
             public_send(scope_name, *args, &blk)
           end
