a96a83e5537b244155b0a36fe0262fa8cf126853
Shinnosuke Iwaide <shinnosuke_iwaide@linc-well.com>
2025-12-03 22:14:30 +0900

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "projects#index"
-  resources :projects, only: [:index]
+  resources :projects, only: [ :index ]
 end
